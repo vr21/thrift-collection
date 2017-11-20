c4c073aa67f31d1c7cf3d50b93e46e916199e6dc
vr21 <reyes_victoria@hotmail.com>
2017-11-20 18:08:38 +0000

    Added a contact route to project

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   root to: 'products#index'
   get 'about', to: 'company_info#about', as:'about'
+  get 'contact', to:'company_info#contact', as: 'contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
